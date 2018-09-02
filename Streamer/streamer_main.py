2eb4779dc8f41347a1b415b6024ebabe35117925
hitman996 <naeemiqbal996@gmail.com>
2018-09-02 22:17:33 +0200

    Added a basic labelling tool. And some assets for html file.

--- Streamer/streamer_main.py
+++ Streamer/streamer_main.py
@@ -3,7 +3,7 @@
 import sys
 import time
 import signal
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request, redirect, url_for, jsonify
 from camera import VideoCamera
 import cv2
 
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index2.html')
+    return render_template('robot2.html')
 
 def gen(camera):
     while True:
@@ -23,6 +23,25 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
+@app.route('/Move_forward')
+def move_forward():
+    forward_message = "Moving Forward..."
+    print forward_message
+    return forward_message
+
+@app.route("/forward", methods=['GET' , 'POST'])
+def move_forward2():
+    #Moving forward code
+    forward_message = "Moving Forward..."
+    return render_template('robot2.html', message=forward_message);
+
+@app.route('/square/', methods=['POST'])
+def square():
+	num = float(request.form.get('number', 0))
+	square = num ** 2
+	data = {'square': square}
+	data = jsonify(data)
+	return data
 
 @app.route('/video_feed')
 def video_feed():
