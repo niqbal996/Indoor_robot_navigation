f0d7ae218f7514e190117634a48e1ba87acc8299
Naeem Iqbal <naeemiqbal996@gmail.com>
2019-01-18 15:32:24 +0100

    Fixed labelling tool bug taking the same frame again and again and not moving the iterator. Also added a trained model for testing purpose.

--- Binary_classification/labelling_tool.py
+++ Binary_classification/labelling_tool.py
@@ -15,28 +15,30 @@ from os import path
 import cv2
 from matplotlib import pyplot as plt
 
-vid = cv2.VideoCapture('test.mp4')
-output_negative = '/home/shaatal/robo/Object-detection-streamer/trainer/images/door/'
-output_positive = '/home/shaatal/robo/Object-detection-streamer/trainer/images/notdoor'
+video_name = 'Log_video_5.avi'
+vid = cv2.VideoCapture(video_name)
+output_negative = '..//trainer//images//notdoor//'
+output_positive = '..//trainer//images//notdoor//'
 height = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
 width = vid.get(cv2.CAP_PROP_FRAME_WIDTH)
 
 print('The video resolution is: ',height , width)
 frame_number = 0
 while(True):
+    vid.set(190, frame_number)            #Read the given frame number instead of reading the first frame again and again
     ret, frame = vid.read()
     frame_number = frame_number +1
     if ret==True:
         for section in range(1,5):
-            section1 = frame[:,(((width / 4)*(section-1))):((width / 4)*section)]
-            filename = str(section)+str(frame_number)+'.jpg'
+            section1 = frame[:,int((width / 4)*(section-1)):int((width / 4)*section)]
+            filename = str(section)+'__'+video_name+'__'+str(frame_number)+'.jpg'
             plt.imshow(section1)
             check = plt.waitforbuttonpress()
             if check==True:
                 cv2.imwrite(path.join(output_positive , filename), section1)
-                print ('Positive')
+                print ('Frame number', frame_number ,'Positive')
             else:
                 cv2.imwrite(path.join(output_negative, filename), section1)
-                print('Negative')
+                print('Frame number', frame_number ,'Negative')
             continue
 
